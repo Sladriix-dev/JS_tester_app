3ed5a83a25fd167c2a6322afccda62da1b683e2c
Sladriix-dev <sladriix@gmail.com>
2022-12-06 17:43:44 +0100

    [remove] Les fichiers tests déjà existant

--- js/samples/unit/unit1.test.js
+++ js/samples/unit/unit1.test.js
@@ -1,18 +1,8 @@
-// Vous importez la fonction à tester
-import { sayHello } from './unit1.js'
+import { sayHello } from './unit1'
 
+describe('SayHello Unit Test Suites', () => {
+    it('should return "Hello, World', () => {
+        expect(sayHello()).toBe("Hello, World")
+    })
 
-// Puis, vous créez le bloc de séries de test (ou Test Suite)
-describe('sayHello Unit Test Suites', () => {
-    it('should display "Hello, World"', () => {
-        expect(sayHello()).toEqual("Hello, World")
-    })
-    
-    it('should display "Bonjour Alexandra"', () => {
-        expect(sayHello("Alexandra")).toEqual("Bonjour Alexandra")
-    })
-    
-    it('should display "Hello, Thomas"', () => {
-        expect(sayHello("Thomas")).toEqual("Hello, Thomas")
-    })
 })
